472730f03159fbdc0d4ad32cb373c5b0b07b5123
chris.g.morton@elmutility.com <chris.g.morton@elmutility.com>
2022-09-29 15:27:30 -0600

    Added tasks to front end via service

--- restfulTaskAPI/public/src/app/app.component.ts
+++ restfulTaskAPI/public/src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
+
+interface ITask  {
+  completed: boolean;
+  createdAt: Date;
+  description: string;
+  title: string;
+  updatedAt: Date;
+  __v: number;
+  _id: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'public';
   welcome = "Here is a welcome message from the ts file";
+  tasks: ITask[] = []
+  oneTask: any = {};
   constructor(private _httpService: HttpService){}
+  ngOnInit() {
+      this.getTasksFromService();
+      this.getOneTaskFromService();
+  }
+  getTasksFromService() {
+    const observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("Data from observable : ", data);
+      this.tasks = data as ITask[];
+      console.log("Tasks as tasks : ", this.tasks);
+    })
+  }
+
+  getOneTaskFromService() {
+    const taskObservable = this._httpService.getOneTask('6335b3d3b79d48200051c7e3');
+    taskObservable.subscribe(data => {
+      this.oneTask  = data;
+      console.log("This one task ", this.oneTask);
+    })
+  }
 }
--- restfulTaskAPI/public/src/app/http.service.ts
+++ restfulTaskAPI/public/src/app/http.service.ts
@@ -7,20 +7,19 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
 
   constructor(private _http: HttpClient) {
-    this.getTasks();
-    this.getOneTask('6335b568c3a8cc041e632958');
   }
   
   getTasks() {
-    const tempObservable = this._http.get('/task');
+    // const tempObservable = this._http.get('/task');
+    return this._http.get('/task');
 
-    tempObservable.subscribe(data => console.log("Got our Tasks! ", data));
+    // tempObservable.subscribe(data => console.log("Got our Tasks! ", data));
   }
 
   getOneTask(taskId: String) {
-    const oneTaskObservable = this._http.get(`/task/${taskId}`);
+    return this._http.get(`/task/${taskId}`);
 
-    oneTaskObservable.subscribe(data => console.log("Got the Task! ", data));
+    // oneTaskObservable.subscribe(data => console.log("Got the Task! ", data));
   }
 }
 
